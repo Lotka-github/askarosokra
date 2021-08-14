33abe51726bdf18af79f78d934c3e10d76ad0bb0
Lotka-github <80006318+Lotka-github@users.noreply.github.com>
2021-08-14 13:52:48 +0200

    Initial commit

---
+++ app/src/main/java/com/example/xposedsampleclock/myModule.java
@@ -0,0 +1,16 @@
+package com.example.xposedsampleclock;
+
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import de.robv.android.xposed.XposedBridge;
+
+
+
+public class myModule implements IXposedHookLoadPackage {
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("Loaded app: " + lpparam.packageName);
+    }
+}
